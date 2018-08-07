865305156a2004c829cb874eb4230df1564f9458
Sharmaine Requerme <srqrme@gmail.com>
2018-08-07 19:25:22 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total
 
-  def initialize(total = 0, employee_discount = nil)
+  def initialize(total = 0, employee_discount = 20)
     @total = total
     @employee_discount = employee_discount
   end
